931f6301b81b1ef16403a789744223b825665005
Ta Thanh Lam <tathanhlam66@gmail.com>
2017-09-15 06:32:56 +0700

    msm8960-common: Add Bluetooth HIDL interfaces

--- msm8960.mk
+++ msm8960.mk
@@ -51,6 +51,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbt-vendor
 
+# Bluetooth HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8960 \
